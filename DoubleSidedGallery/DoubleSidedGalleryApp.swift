c8fc62d21ece2909f1c6da7bcab3d77eb260b205
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-10-10 22:15:14 +0200

    Initial Commit

---
+++ DoubleSidedGallery/DoubleSidedGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleSidedGalleryApp.swift
+//  DoubleSidedGallery
+//
+//  Created by Michele Manniello on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DoubleSidedGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
